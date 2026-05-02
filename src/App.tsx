556dc316c5bc2687912e442b1ce527ef6b3a9712
Muhammad Fayaz <mfayaz21703@gmail.com>
2026-05-02 12:18:09 +0500

    refactor: comment out RouterProvider and related code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router"; // apna correct path
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./router/router"; // apna correct path
 
-const App: React.FC = () => {
-  return <RouterProvider router={router} />;
-};
+// const App: React.FC = () => {
+//   return <RouterProvider router={router} />;
+// };
 
-export default App;
+// export default App;
