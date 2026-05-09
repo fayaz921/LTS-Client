68d24145f825ec258414106bddfedd9dc8ed2a04
MuhammadShehzad709 <muhammadshehzaddeveloper@gmail.com>
2026-05-09 20:23:50 +0500

    Changing path
    here i have changed the path for case documetns and alerts

--- src/features/alerts/api/alertsApi.ts
+++ src/features/alerts/api/alertsApi.ts
@@ -1,4 +1,5 @@
 import instance from '../../../lib/axios'
+import type { ApiResponse } from '../../../shared/types/api.types';
 import type { GetUpComingHearingDto } from '../types'
 
 export const alertsApi = {
@@ -6,16 +7,16 @@ export const alertsApi = {
     // GET /api/alerts/GetUpComingHearing
     getUpcomingHearings: () =>
         instance
-            .get<{ status: number; message: string; data: GetUpComingHearingDto[] }>(
-                '/api/alerts/GetUpComingHearing'
+            .get<ApiResponse<GetUpComingHearingDto[]>>(
+                '/alerts/GetUpComingHearing'
             )
             .then(r => r.data.data),
 
     // POST /api/alerts/SendHearingAlert{CaseId}
     sendAlert: (caseId: string) =>
         instance
-            .post<{ status: number; message: string; data: boolean }>(
-                `/api/alerts/SendHearingAlert${caseId}`
+            .post<ApiResponse<boolean>>(
+                `/alerts/SendHearingAlert${caseId}`
             )
             .then(r => r.data),
 }
